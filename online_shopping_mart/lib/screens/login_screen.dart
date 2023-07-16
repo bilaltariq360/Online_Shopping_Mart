1111d8f75160ac142b9f0e1b76f395ca84c32f94
Bilal Tariq <hafizbilaltariq360@gmail.com>
2023-07-16 23:02:34 +0500

    Upadted

--- online_shopping_mart/lib/screens/login_screen.dart
+++ online_shopping_mart/lib/screens/login_screen.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:online_shopping_mart/screens/products_screen.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static const routeName = '/login';
 
-  Widget inputFieldBuild(String label, IconData icon) {
+  LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController username = TextEditingController();
+
+  TextEditingController password = TextEditingController();
+
+  bool isError = false;
+
+  Widget inputFieldBuild(String label, IconData icon, bool isPassword) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
       child: TextField(
+        controller: (label == 'Username') ? username : password,
+        obscureText: (isPassword) ? true : false,
         decoration: InputDecoration(
           labelText: label,
           prefixIcon: Icon(
@@ -33,7 +48,7 @@ class LoginScreen extends StatelessWidget {
           ),
         ),
         style: const TextStyle(
-          fontSize: 17,
+          fontSize: 22,
           letterSpacing: 1,
           color: Color.fromRGBO(208, 255, 0, 1),
         ),
@@ -41,74 +56,104 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  LoginScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         color: const Color.fromRGBO(22, 22, 22, 1),
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/online-shopping-mart.png',
-              width: 150,
-            ),
-            const Text(
-              'Verify first',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Color.fromRGBO(208, 255, 0, 1),
-              ),
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-            inputFieldBuild('Username', Icons.account_circle),
-            inputFieldBuild('Password', Icons.password),
-            const SizedBox(
-              height: 50,
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-              width: double.infinity,
-              height: 50,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushReplacementNamed(ProductsScreen.routeName);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromRGBO(
-                      208, 255, 0, 1), // Set the button's background color
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(30), // Set the border radius
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 40),
+                  child: Image.asset(
+                    'assets/images/online-shopping-mart.png',
+                    width: 150,
                   ),
                 ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.arrow_circle_right_outlined,
-                      color: Color.fromRGBO(22, 22, 22, 1),
-                      size: 25,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      'Login',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Color.fromRGBO(22, 22, 22, 1),
+                const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(208, 255, 0, 1),
+                  ),
+                ),
+                (isError)
+                    ? Container(
+                        margin: const EdgeInsets.only(top: 40),
+                        child: const Text(
+                          'Invalid username or password',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.red,
+                          ),
+                        ),
+                      )
+                    : const SizedBox(
+                        width: 0,
+                      ),
+                const SizedBox(
+                  height: 40,
+                ),
+                inputFieldBuild('Username', Icons.account_circle, false),
+                inputFieldBuild('Password', Icons.password, true),
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (username.text == 'clickcart' &&
+                          password.text == 'bilal') {
+                        setState(() {
+                          isError = false;
+                        });
+                        Navigator.of(context)
+                            .pushReplacementNamed(ProductsScreen.routeName);
+                      } else {
+                        setState(() {
+                          isError = true;
+                        });
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(208, 255, 0, 1),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
                       ),
                     ),
-                  ],
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.arrow_circle_right_outlined,
+                          color: Color.fromRGBO(22, 22, 22, 1),
+                          size: 25,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Login',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromRGBO(22, 22, 22, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
